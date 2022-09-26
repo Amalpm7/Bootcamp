07d13690d0be71e1e32de1890895ec89bfb11fee
Amalpm7 <amal.p@nestgroup.net>
2022-09-26 17:14:01 +0530

    Array list

--- src/com/Nest/Employee/Employee.java
+++ src/com/Nest/Employee/Employee.java
@@ -1,10 +1,13 @@
 package com.Nest.Employee;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Employee {
     public static void main(String[] args) {
         int choice;
+        String empName;
+        ArrayList<String> empList = new ArrayList<>();
 
         while (true){
             System.out.println("Select an option");
@@ -12,17 +15,19 @@ public class Employee {
             System.out.println("2. View Employee");
             System.out.println("3. Exit");
 
-            Scanner opt=new Scanner(System.in);
+            Scanner opt =new Scanner(System.in);
             choice=opt.nextInt();
 
 
 
             switch (choice){
                 case 1:
-                    System.out.println("Add employee");
+                    System.out.println("Enter the name of employee");
+                    empName = opt.next();
+                    empList.add(empName);
                     break;
                 case 2:
-                    System.out.println("View employee");
+                    System.out.println(empList);
                     break;
                 case 3:
                     System.exit(0);
